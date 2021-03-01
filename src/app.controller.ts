96e6939aff9171b8db32019aae5caabf0991c190
Thai Loc <kunkaamd@gmail.com>
2021-03-01 17:29:54 +0700

    update comment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -21,7 +21,7 @@ export class AppController {
     return this.authService.login(req.user);
   }
 
-  @ApiBearerAuth()
+  @ApiBearerAuth()//this for swagger
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
